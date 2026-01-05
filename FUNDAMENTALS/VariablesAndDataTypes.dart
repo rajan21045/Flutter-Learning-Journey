15b5418d19c6340cf465b6ad592f62d954a45b0b
Rajan Poudel <raajan.works@gmail.com>
2026-01-05 20:31:38 +0545

    Add files via upload

---
+++ FUNDAMENTALS/VariablesAndDataTypes.dart
@@ -0,0 +1,18 @@
+void main() {
+  // Variable Declarations and Data Types in Dart
+  int age = 20;
+  String name = "Rajan";
+  double height = 5.8;
+  bool isStudent = true;
+  print("Name: $name");
+  print("Age: $age");
+  print("Height: $height");
+  print("Is Student: $isStudent");
+
+  //Type Inference using var
+  //Once assigned, the type cannot change.
+  var age2 = 20;
+  var name2 = "Rajan";
+  print("Name2: $name2");
+  print("Age2: $age2");
+}
